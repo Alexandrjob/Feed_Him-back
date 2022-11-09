9cce574ecc392dde9f18470c1dfce441cd536e3b
Alexandr Savarskiy <sshkajob@gmail.com>
2022-11-09 20:32:07 +0700

    Добавлена возможность настраивать количество порций в день. Добавлена генерация таблицы config. Добавлена миграция таблицы config. Добавлена gitignore. Мелкие исправления касающиеся больше читабельности кода.

---
+++ src/TaskTrackerCat.Migrator/Magrations/0002_ConfigTableMs.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace TaskTrackerCat.Migrator.Magrations;
+
+[Migration(2)]
+public class ConfigTableMs : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"
+            IF NOT EXISTS (
+                SELECT * FROM sys.tables t 
+                JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+                WHERE s.name = 'config') 	
+                CREATE TABLE config(
+                    id INT PRIMARY KEY IDENTITY,
+                    number_meals_per_day INT,
+                    );");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql("IF EXISTS (" +
+                    "DROP TABLE dbo.config" +
+                    ");");
+    }
+}
---
+++ src/TaskTrackerCat/Repositories/Interfaces/IConfigRepository.cs
@@ -0,0 +1,20 @@
+﻿using TaskTrackerCat.Repositories.Models;
+
+namespace TaskTrackerCat.Repositories.Interfaces;
+
+public interface IConfigRepository
+{
+    
+    /// <summary>
+    /// Обновление количества приемов еды.
+    /// </summary>
+    /// <param name="config">Класс dto.</param>
+    /// <returns></returns>
+    public Task UpdateConfigAsync(ConfigDto config);
+
+    /// <summary>
+    /// Получение конфигурации.
+    /// </summary>
+    /// <returns>Класс dto.</returns>
+    Task<ConfigDto> GetConfigAsync(ConfigDto config);
+}
